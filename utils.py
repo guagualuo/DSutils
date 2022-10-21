b3e540713401f5618bdd2941cfbe89734fc59e60
guagualuo <paulljw333@gmail.com>
2022-10-21 15:32:19 +0100

    add function to convert pandas Dataframe or Series to numpy array

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def _random_state(seed) -> np.random.RandomState:
@@ -10,3 +11,12 @@ def _random_state(seed) -> np.random.RandomState:
         return seed
     else:
         raise RuntimeWarning(f"random seed {seed} not accepted")
+
+
+def _to_numpy(arr):
+    if isinstance(arr, np.ndarray):
+        return arr
+    elif isinstance(arr, (pd.DataFrame, pd.Series)):
+        return arr.to_numpy()
+    else:
+        raise RuntimeWarning("array is not one of array, DataFrame, Series.")
